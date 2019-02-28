da4455e5cd49225c4a239ee3987bbf5f95778802
sontranrad <sontranrad@gmail.com>
2019-02-28 18:06:07 +0700

    Ft/add partial matching logic for exchange smc (#271)
    * Add partial matching logic and interfaces to query tx
    
    * Update logic calculating available amount after matching
    
    * Add partial matching logic and remove unused functions
    
    * Enhance logic for partial matching in smc
    
    * Update new complete order logic
    
    * Update bytecode and abi in config files
    
    * Update status of both 2 orders when complete one
    
    * Fix error complete order return wrong status

--- kvm/smc/ExchangeV2.sol
+++ kvm/smc/ExchangeV2.sol
@@ -1,13 +1,25 @@
 pragma solidity ^0.4.24;
 contract KardiaExchange {
+    struct ReleaseInfo {
+        string matchedOriginalTxId;
+        string pair;
+        string receiveAddress;
+        string releaseTxId;
+        uint256 releaseAmount;
+        uint status;
+    }
+
     struct ExchangeOrder {
         string pair;
         string fromAddress;
         string toAddress;
         string originalTxId;
         uint256 sellAmount;
+        // receiveAmount is total amount to receive, unit is in destination currency
         uint256 receiveAmount;
-        string matchedOrderId;
+        // availableAmount is unmatched Amount, unit is in source currency
+        uint256 availableAmount;
+        // need to add matched and unmatched amount here;
         uint done;
         bool added;
     }
@@ -16,12 +28,11 @@ contract KardiaExchange {
         uint sellAmount;
         uint receiveAmount;
     }
-
+    mapping (string => ReleaseInfo[]) releasesByID;
     mapping (string => ExchangeOrder) listOrders;
     mapping (string => string[]) orderIDsByPairs;
     mapping (string => Rate) rates;
     mapping (string => string[]) orderIDsByAddress;
-    uint256 counter;
     event Release(
         string indexed pair,
         string indexed addr,
@@ -46,38 +57,52 @@ contract KardiaExchange {
     // create an order with source - dest address, source - dest pair and amount
     // order will be stored with returned ID in smc
     // order from ETH from NEO should be "ETH-NEO", "NEO-ETH"
-    function matchOrder(string srcPair, string destPair, string srcAddress, string destAddress, string originalTxId, uint256 amount) public returns (uint256) {
+    function matchOrder(string srcPair, string destPair, string srcAddress, string destAddress, string originalTxId, uint256 amount) public {
         Rate memory r = getRate(srcPair);
-        if (r.receiveAmount == 0 || r.sellAmount == 0 ) {
-            return 0;
+        if (listOrders[originalTxId].added) {
+            return;
         }
-        ++counter;
+        if (r.receiveAmount == 0 || r.sellAmount == 0 ) {
+            return;
+        }
         uint256 receiveAmount = amount * r.receiveAmount / r.sellAmount;
-        string memory matchOrderId = findMatchingOrder(destPair, receiveAmount);
-        ExchangeOrder memory order = ExchangeOrder(srcPair, srcAddress, destAddress, originalTxId, amount, receiveAmount, matchOrderId, 0, true);
+        ReleaseInfo[] releases;
+        uint256 totalMatched;
+        ExchangeOrder memory order = ExchangeOrder(srcPair, srcAddress, destAddress, originalTxId, amount, receiveAmount, amount , 0, true);
+        if (orderIDsByPairs[destPair].length > 0 ) {
+            (releases, totalMatched) = findPartialMatchingOrders(originalTxId, srcPair, destPair, orderIDsByPairs[destPair], r, receiveAmount, destAddress);
+            if (totalMatched > 0) {
+                // calculate totalMatched back to original unit
+                order.availableAmount = amount - (totalMatched * r.sellAmount / r.receiveAmount);
+            }
+        }
+        for (uint i = 0 ; i < releases.length; i ++) {
+            releasesByID[originalTxId].push(releases[i]);
+        }
+        // we need to add matched order of original direction here too update available amount of newly added order
         listOrders[originalTxId] = order;
         orderIDsByPairs[srcPair].push(originalTxId);
         orderIDsByAddress[srcAddress].push(originalTxId);
-        if (keccak256(abi.encodePacked(order.matchedOrderId)) != keccak256(abi.encodePacked("")) ) {
-            listOrders[matchOrderId].matchedOrderId = originalTxId;
-            emit Release(destPair, listOrders[matchOrderId].toAddress, matchOrderId, listOrders[matchOrderId].receiveAmount);
-        }
-        return counter;
     }
 
-    // find matching order for targeted pair with specific amount, return global order ID
-    function findMatchingOrder(string destPair, uint256 receiveAmount) internal view returns (string) {
-        string[] memory ids = orderIDsByPairs[destPair];
-        for (uint256 i = 0; i < ids.length; i++) {
-            ExchangeOrder memory order = listOrders[ids[i]];
-            if (order.sellAmount == receiveAmount
-                && keccak256(abi.encodePacked(order.matchedOrderId)) == keccak256(abi.encodePacked(""))
-                && order.done == 0) {
-                return ids[i];
-            }
+    // find matching order for targeted pair with specific amount, return global order ID and total matched amount in dest currency
+    function findPartialMatchingOrders(string originalTxId, string sourcePair, string destPair, string[] ids,  Rate r, uint256 receiveAmount, string receiveAddress) internal returns (ReleaseInfo[] storage releases, uint256 totalMatched ) {
+        uint256 i = 0;
+        while (i < ids.length && totalMatched < receiveAmount ) {
+            // if sourcePair is ETH-NEO, matchableAmount unit is NEO
+            uint256 matchableAmount = receiveAmount > listOrders[ids[i]].availableAmount ? listOrders[ids[i]].availableAmount : receiveAmount;
+            ReleaseInfo memory r1 = ReleaseInfo(originalTxId, sourcePair, receiveAddress, "", matchableAmount, 0);
+            releases.push(r1);
+            uint256 matchableAmount2 = matchableAmount * r.sellAmount / r.receiveAmount;
+            // update availableAmount of matched order, if matchOrder is NEO-ETH, availableAmount unit is NEO)
+            listOrders[ids[i]].availableAmount -= matchableAmount;
+            ReleaseInfo memory r2 = ReleaseInfo(ids[i], destPair, listOrders[ids[i]].toAddress, "", matchableAmount2, 0);
+            releasesByID[ids[i]].push(r2);
+            releases.push(r2);
+            totalMatched += matchableAmount;
+            i++;
         }
-        // no matching order ID found
-        return "";
+        return (releases, totalMatched);
     }
 
     // Get order id from its details:
@@ -93,46 +118,34 @@ contract KardiaExchange {
         return "";
     }
 
-    // Get matched order detail for a specific order
-    function getMatchingOrderInfo(string orderID) public view returns (string matchedOrderID, string destAddress, uint256 sendAmount) {
-        if (listOrders[orderID].added == false) {
-            return ("", "", 0);
-        }
-
-        string memory matchedId = listOrders[orderID].matchedOrderId;
-        if (keccak256(abi.encodePacked(matchedId)) != keccak256(abi.encodePacked(""))) {
-            ExchangeOrder memory matchedOrder = listOrders[matchedId];
-            return (matchedId, matchedOrder.toAddress, matchedOrder.receiveAmount);
-
-        }
-        // no matching order found
-        return ("", "", 0);
-    }
-
     // Complete a order indicates that the order with orderID has been release successfully
     // TODO(@sontranrad): implement retry logic in case release is failed
-    function completeOrder(string orderID, string pair) public returns (uint success){
-        ExchangeOrder memory order = listOrders[orderID];
-        if (keccak256(abi.encodePacked(order.pair)) != keccak256(abi.encodePacked(pair)) ) {
+    function completeOrder(string originalTxId, string releaseTxId, string pair, string receiveAddress, uint256 releaseAmount) public returns (uint success){
+        ReleaseInfo[] releases = releasesByID[originalTxId];
+        if (releases.length == 0) {
             return 0;
         }
-        if (listOrders[orderID].done != 0) {
-            return 0;
+        for (uint i = 0; i < releases.length; i++) {
+            if (keccak256(abi.encodePacked(pair)) == keccak256(abi.encodePacked(releases[i].pair)) &&
+                keccak256(abi.encodePacked(receiveAddress)) == keccak256(abi.encodePacked(releases[i].receiveAddress)) &&
+                releaseAmount == releases[i].releaseAmount) {
+                releases[i].releaseTxId = releaseTxId;
+                releases[i].status = 1;
+                string matchedTxId = releases[i].matchedOriginalTxId;
+                // ReleaseInfo[] storage opponents = releasesByID[matchedTxId];
+                if (releasesByID[matchedTxId].length > 0) {
+                    for (uint j = 0; j < releasesByID[matchedTxId].length; j++) {
+                        if (keccak256(abi.encodePacked(releasesByID[matchedTxId][j].receiveAddress))
+                            == keccak256(abi.encodePacked(receiveAddress)) && releasesByID[matchedTxId][j].releaseAmount == releaseAmount
+                            && releasesByID[matchedTxId][j].status == 0) {
+                            releasesByID[matchedTxId][0].releaseTxId = releaseTxId;
+                            releasesByID[matchedTxId][0].status = 1;
+                        }
+                    }
+                }
+            }
         }
-        listOrders[orderID].done = 1;
-        return 1;
-    }
 
-    // Get exchangeable amount of each pair
-    function getAvailableAmountByPair(string pair) public view returns (uint256 amount) {
-        amount = 0;
-        string[] memory ids = orderIDsByPairs[pair];
-        bytes32 encodedEmptyString = keccak256(abi.encodePacked(""));
-        for (uint i = 0; i < ids.length; i++) {
-            if (keccak256(abi.encodePacked(listOrders[ids[i]].matchedOrderId)) == encodedEmptyString)
-                amount += listOrders[ids[i]].receiveAmount;
-        }
-        return amount;
     }
 
     // Get 10 matchable amount by pair
@@ -147,16 +160,23 @@ contract KardiaExchange {
         return amounts;
     }
 
-    // Get the opposite uncompleted order of a compelete order
-    function getUncompletedMatchingOrder(string orderID) public view returns (string matchedOrderID, string destAddress, uint256 sendAmount) {
-        string memory oppositeID = listOrders[orderID].matchedOrderId;
-        if (keccak256(abi.encodePacked(oppositeID)) == keccak256(abi.encodePacked("")) ) {
-            return ("", "", 0);
+    // Get release by originalTxId and pair
+    function getReleaseByTxId(string originalTxId, string pair) public view returns (string releaseInfos) {
+        ReleaseInfo[] memory releases = releasesByID[originalTxId];
+        if (releases.length == 0) {
+            releaseInfos = "";
+            return releaseInfos;
         }
-        ExchangeOrder memory oppositeOrder = listOrders[oppositeID];
-        if (oppositeOrder.done == 0) {
-            return (oppositeID, oppositeOrder.toAddress, oppositeOrder.receiveAmount);
+        for (uint i = 0; i < releases.length; i++) {
+            if (keccak256(abi.encodePacked(releases[i].pair)) == keccak256(abi.encodePacked(pair))) {
+                if (keccak256(abi.encodePacked(releaseInfos)) == keccak256(abi.encodePacked(""))) {
+                    releaseInfos = releaseInfoToString(releases[i]);
+                } else {
+                    releaseInfos = string(abi.encodePacked(releaseInfos, "|", releaseInfoToString(releases[i])));
+                }
+            }
         }
+        return releaseInfos;
     }
 
     function orderToString(ExchangeOrder r ) internal pure returns (string) {
@@ -166,8 +186,14 @@ contract KardiaExchange {
         string memory sell = uint2str(r.sellAmount);
         string memory receive = uint2str(r.receiveAmount);
         string memory done = uint2str(r.done);
-        return string(abi.encodePacked(r.fromAddress, "|", r.toAddress, "|", r.originalTxId,"|", sell, "|", receive,
-            "|", r.matchedOrderId, "|", done));
+        string memory available = uint2str(r.availableAmount);
+        return string(abi.encodePacked(r.pair, ";", r.fromAddress, ";", r.toAddress, ";", r.originalTxId,";", sell, ";", receive, ";", available, ";", done));
+    }
+
+    function releaseInfoToString(ReleaseInfo r ) internal pure returns (string) {
+        string memory amount = uint2str(r.releaseAmount);
+        string memory status = uint2str(r.status);
+        return string(abi.encodePacked(r.matchedOriginalTxId, ":", r.pair, ";", r.receiveAddress, ";", r.releaseTxId, ";", amount,";", status));
     }
 
     function uint2str(uint i) internal pure returns (string){
@@ -187,7 +213,7 @@ contract KardiaExchange {
         return string(bstr);
     }
 
-    function getOrderBook(string pair) public view returns (string) {
+    function getOrderBook(string pair) public view returns (string orderBook) {
         string[] memory ids = orderIDsByPairs[pair];
         uint length = ids.length;
         if (length == 0) {
@@ -206,7 +232,7 @@ contract KardiaExchange {
     }
 
     // get all order history of an address
-    function getOrderHistory(string addr) public view returns (string) {
+    function getOrderHistory(string addr) public view returns (string orderHistory) {
         string[] memory ids = orderIDsByAddress[addr];
         uint length = ids.length;
         if (length == 0) {
@@ -225,7 +251,7 @@ contract KardiaExchange {
     }
 
     // Get all order history of an address and a pair
-    function getOrderHistoryByPair(string addr, string pair) public view returns (string) {
+    function getOrderHistoryByPair(string addr, string pair) public view returns (string orderHistory) {
         string[] memory ids = orderIDsByAddress[addr];
         uint length = ids.length;
         if (length == 0) {
