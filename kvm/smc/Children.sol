e6cee49e4fdfdace166557fc9cd7c2861799da63
sontranrad <sontranrad@gmail.com>
2019-03-16 10:58:07 +0700

    Simplify children contract to reduce cost

--- kvm/smc/Children.sol
+++ kvm/smc/Children.sol
@@ -1,88 +1,33 @@
 pragma solidity ^0.4.24;
 
-import "github.com/Arachnid/solidity-stringutils/strings.sol";
-
 contract Children {
-    using strings for *;
-
     mapping(uint => bool) isSet;
-    mapping(uint => string[]) listAttributes;
-    mapping(uint => string[]) listValues;
-    function splitString(string str, string delimiter) internal returns (string[]) {
-        strings.slice memory s = str.toSlice();
-        strings.slice memory delim = delimiter.toSlice();
-        string[] memory parts = new string[](s.count(delim) + 1);
-        for(uint i = 0; i < parts.length; i++) {
-            parts[i] = s.split(delim).toString();
-        }
-        return parts;
-    }
+    mapping(uint => string) listAttributes;
+    mapping(uint => string) listValues;
 
-    function addChild(uint id, string attributes, string values) public {
-        string[] memory arrAttr = splitString(attributes, "|");
-        string[] memory arrValues = splitString(values, "|");
-        if (arrAttr.length != arrValues.length) {
-            revert();
-        }
-        listAttributes[id] = arrAttr;
-        listValues[id] = arrValues;
+    function addChildren(uint id, string atts, string values) public {
         isSet[id] = true;
+        listAttributes[id] = atts;
+        listValues[id] = values;
     }
 
-    function childToString(uint id) internal returns (string) {
-        string memory result = string(abi.encodePacked("id:", uint2str(id), "|"));
-        if (listAttributes[id].length == 0) {
-            return "";
+    function getChildren(uint _from, uint _to) public view returns (string atts, string values) {
+        if (_from < _to) {
+            return ("","");
         }
-        for (uint i = 0; i < listAttributes[id].length; i++) {
-            string memory field = string(abi.encodePacked(listAttributes[id][i], ":",
-                listValues[id][i]));
-            if (i < listAttributes[id].length - 1) {
-                result = string(abi.encodePacked(result, field, "|"));
-            } else {
-                result = string(abi.encodePacked(result, field));
+        string memory strAttrs;
+        string memory strVals;
+        for (uint i = _from; i <= _to; i++) {
+            if (isSet[i]) {
+                if (i < _to) {
+                    strAttrs = string(abi.encodePacked(strAttrs, listAttributes[i],"|"));
+                    strVals = string(abi.encodePacked(strAttrs, listValues[i], "|"));
+                } else {
+                    strAttrs = string(abi.encodePacked(strAttrs, listAttributes[i]));
+                    strVals = string(abi.encodePacked(strAttrs, listValues[i]));
+                }
             }
         }
-        return result;
-    }
-
-
-    // getChildInfo returns info of a child by id
-    function getChildInfo(uint id) public view returns (string childInfo) {
-        return childToString(id);
-    }
-
-    // getChildren returns info of children from from_id to to_id, separated by ~
-    function getChildren(uint from_id, uint to_id) public view returns (string childrenInfo) {
-        if (from_id > to_id) {
-            return "";
-        }
-        string memory result = "";
-        for (uint i = from_id; i <= to_id; i++) {
-            if (i < to_id) {
-                result = string(abi.encodePacked(result, childToString(i), "~"));
-            }
-            else {
-                result = string(abi.encodePacked(result, childToString(i)));
-            }
-        }
-        return result;
-    }
-    // convert a uint type into string
-    function uint2str(uint i) internal pure returns (string) {
-        if (i == 0) return "0";
-        uint j = i;
-        uint length;
-        while (j != 0){
-            length++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(length);
-        uint k = length - 1;
-        while (i != 0){
-            bstr[k--] = byte(48 + i % 10);
-            i /= 10;
-        }
-        return string(bstr);
+        return (strAttrs, strVals);
     }
 }
