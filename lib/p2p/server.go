f3744e9b0f0a86e4400102b99ec7b45bc749dc66
kien Dang <kien.15@gmail.com>
2019-06-05 01:18:49 +0700

    Decrease Read Write Deadline

--- lib/p2p/server.go
+++ lib/p2p/server.go
@@ -43,10 +43,10 @@ const (
 
 	// Maximum time allowed for reading a complete message.
 	// This is effectively the amount of time a connection can be idle.
-	frameReadTimeout = 300 * time.Second
+	frameReadTimeout = 60 * time.Second
 
 	// Maximum amount of time allowed for writing a complete message.
-	frameWriteTimeout = 200 * time.Second
+	frameWriteTimeout = 40 * time.Second
 )
 
 var errServerStopped = errors.New("server stopped")
